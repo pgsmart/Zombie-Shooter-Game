0139b06dfe3d5c82c0138adab45e1fb4a4912a09
pgsmart <101372312+pgsmart@users.noreply.github.com>
2022-12-09 21:26:37 -0600

    Add files via upload

---
+++ player.js
@@ -0,0 +1,87 @@
+class Players{
+    constructor(){
+        this.rank = null
+        this.coins = null
+        this.index = null
+        this.name = null
+    }
+    addPlayers(name,count){
+        console.log(name)
+        database.ref("/").update({
+            playerCount : count
+        })
+        this.index = playerCount
+      
+        database.ref("/player" + this.index).set({
+            name: name,
+            coins: 0,
+            treasureCollected: false,
+            won: false
+        })
+      
+    }
+    
+    updateCoins(coinTotal){
+        database.ref("/player" + this.index).update({
+            coins : coinTotal
+        })
+    }
+    getCount(){     
+        database.ref("/playerCount").on("value",function(data){
+            playerCount = data.val()
+          /*if(playerCount === 2){
+          extraPlayer()
+          state = "STOP"
+        }*/
+        })
+    }
+  updateTreasure(){
+    database.ref("/player" + this.index).update({
+      treasureCollected : true
+    })
+  }
+  updateOpponent(){
+    var otherIndex = 0;
+    switch(this.index){
+      case 1 : otherIndex = 2
+        break;
+      case 2 : otherIndex = 1
+        break;
+    }
+    database.ref("/player" + otherIndex + "/name").on("value",function(data){
+      opponentNameTxt.html(`${data.val()}`)
+    })
+    database.ref("/player" + otherIndex + "/treasureCollected").on("value",function(data){
+      if(data.val() === true){
+        otherTreasure.addImage(treasureImg)
+        gameOver('treasure')
+      }
+    })
+    database.ref("/player" + otherIndex + "/coins").on('value',function(data){
+      console.log(data.val())
+      switch(data.val()){
+        case 0 : 
+          otherCoin1.addImage(coinClearImg)
+          otherCoin2.addImage(coinClearImg)
+          otherCoin3.addImage(coinClearImg)
+          break;
+        case 1 : 
+          otherCoin1.addImage(coinFullImg)
+          otherCoin2.addImage(coinClearImg)
+          otherCoin3.addImage(coinClearImg)
+          break;
+        case 2 : 
+          otherCoin1.addImage(coinFullImg)
+          otherCoin2.addImage(coinFullImg)
+          otherCoin3.addImage(coinClearImg)
+          break;
+        case 3 : 
+          otherCoin1.addImage(coinFullImg)
+          otherCoin2.addImage(coinFullImg)
+          otherCoin3.addImage(coinFullImg)
+          break;
+      }
+    })
+  }
+
+}
--- sketch.js
+++ sketch.js
@@ -9,6 +9,34 @@ var boxPositions = [];
 var zombiePositions = [];
 var coinImg;
 var coin;
+var health = 3;
+var healthImg1;
+var healthImg2;
+var healthImg3;
+var healthSprite;
+var coinsCollected = 0;
+var treasure, treasureImg;
+var percent = boxPositions.length/45
+var state = "WAIT"
+var input, playButton;
+var newPlayer;
+var database;
+var sound;
+var soundButton;
+var mute = false;
+var coin1,coin2,coin3;
+var coinClearImg;
+var coinFullImg;
+var treasureOpenImg;
+var bulletRightImg, bulletLeftImg;
+var treasure1, treasureClearImg;
+var playerCount;
+var waitText;
+var loading;
+var otherCoin1, otherCoin2, otherCoin3;
+var otherCoinsCollected;
+var opponentNameTxt;
+var otherTreasure;
 function preload() {
 
   shooterImg = loadImage("assets/shooter_2.png")
@@ -21,18 +49,36 @@ function preload() {
   zombieImg = loadImage("assets/zombie.png")
   zombieImg2 = loadImage("assets/zombie-2.png")
 
-  coinImg = createImg("assets/coin.gif")
+  bulletRightImg = loadImage("assets/bulletRight.png")
+  bulletLeftImg = loadImage("assets/bulletLeft.png")
+
+  coinImg = loadAnimation("assets/coin/sprite_0.png","assets/coin/sprite_1.png","assets/coin/sprite_2.png","assets/coin/sprite_3.png","assets/coin/sprite_4.png","assets/coin/sprite_5.png")
+
+  healthImg1 = loadImage("assets/heart_1.png")
+  healthImg2 = loadImage("assets/heart_2.png")
+  healthImg3 = loadImage("assets/heart_3.png")
+
+  treasureImg = loadImage("assets/treasure.png")
+  treasureOpenImg = loadImage("assets/treasureOpen.png")
+
+  sound = loadSound("assets/a-robust-crew.mp3")
+
+  coinClearImg = loadImage("assets/coinTransparent.png")
+  coinFullImg = loadImage("assets/coin/sprite_1.png")
+  treasureClearImg = loadImage("assets/treasureClear.png")
 }
 
 function setup() {
-
+  database = firebase.database();
 
   createCanvas(windowWidth, windowHeight);
 
+  newPlayer = new Players()
+  newPlayer.getCount()
+  console.log(newPlayer.index)
+
   zombieGroup = createGroup()
   bulletGroup = createGroup()
-
-  moveCoin()
 
   //creating the player sprite
   player = createSprite(displayWidth - 1150, displayHeight - 300, 50, 50);
@@ -42,6 +88,83 @@ function setup() {
   player.debug = true
   player.setCollider("rectangle", 0, 0, 300, 300)
 
+  opponentNameTxt = createElement('h1')
+  opponentNameTxt.position(width* 0.85,height * 0.30)
+  opponentNameTxt.style('color','#ffffff')
+  opponentNameTxt.hide()
+
+  healthSprite = createSprite(width/2,height/12,40,40)
+  healthSprite.addImage(healthImg3)
+  healthSprite.scale = 0.3
+
+  coin = createSprite(width/2,height/2,20,20)
+  coin.addAnimation("coin",coinImg)
+  coin.scale = 0.6
+  coin.setCollider("rectangle",0,0,100,100)
+
+  coin1 = createSprite(width/4,height/15)
+  coin1.addImage(coinClearImg)
+  coin1.scale = 0.5
+  coin2 = createSprite(width/4 + 75,height/15)
+  coin2.addImage(coinClearImg)
+  coin2.scale = 0.5
+  coin3 = createSprite(width/4 + 150,height/15)
+  coin3.addImage(coinClearImg)
+  coin3.scale = 0.5
+  treasure1 = createSprite(width/4 + 250,height/15)
+  treasure1.addImage(treasureClearImg)
+  treasure1.scale = 0.15
+
+  otherCoin1 = createSprite(width * 0.80,height * 0.50)
+  otherCoin1.addImage(coinClearImg)
+  otherCoin1.scale = 0.5
+  otherCoin2 = createSprite(width * 0.85,height * 0.50)
+  otherCoin2.addImage(coinClearImg)
+  otherCoin2.scale = 0.5
+  otherCoin3 = createSprite(width * 0.90,height * 0.50)
+  otherCoin3.addImage(coinClearImg)
+  otherCoin3.scale = 0.5
+  otherCoin1.visible = false;
+  otherCoin2.visible = false;
+  otherCoin3.visible = false;
+  otherTreasure = createSprite(width * 0.80,height*0.60)
+  otherTreasure.addImage(treasureClearImg)
+  otherTreasure.scale = 0.15
+  otherTreasure.visible = false;
+
+  treasure = createSprite(Math.round(random(0,8))* height/7 + width/5,Math.round(random(0,4)) * height/7 + height/4,50,50)
+  treasure.addImage(treasureImg)
+  treasure.scale = 0.2
+  treasure.visible = false;
+  treasure.setCollider("rectangle",0,0,500,500)
+
+  moveCoin()
+
+  input = createInput()
+  input.position(width * 0.90,height * 0.30)
+  input.size(100,50)
+
+  playButton = createButton("Play")
+  playButton.mousePressed(playSound)
+  playButton.position(width * 0.90,400)
+  playButton.size(100,50)
+
+  waitText = createElement('h1')
+  waitText.hide()
+  waitText.style('color','#f2f2f0')
+  waitText.position(width * 0.83,height * 0.40)
+
+  soundButton = createButton("Mute/Unmute")
+  soundButton.position(7 * width/8,height/12)
+  soundButton.size(100,30)
+  soundButton.mousePressed(soundChange)
+  
+  loading = createElement('img')
+  loading.style('content','url(/assets/loading.gif)')
+  loading.position(width * 0.83,height * 0.50)
+  loading.size(height/4,height/4)
+  loading.hide()
+
 }
 
 function draw() {
@@ -50,28 +173,76 @@ function draw() {
 
   image(bgImg, 0, 0, windowWidth, windowHeight)
 
+  if(playerCount === 2){
+    state = "PLAY"
+    startGame();
+  }
 
   //moving the player up and down and making the game mobile compatible using touches
-  if (keyDown("UP_ARROW") || touches.length > 0) {
-    player.y = player.y - 30
+  if (keyDown("UP_ARROW") && player.y > height / 4 && state === "PLAY") {
+    player.y = player.y - 20
   }
-  if (keyDown("DOWN_ARROW") || touches.length > 0) {
-    player.y = player.y + 30
+  if (keyDown("DOWN_ARROW") && player.y < height * 8/10 && state === "PLAY") {
+    player.y = player.y + 20
   }
 
-  if (keyDown("LEFT_ARROW")) {
-    player.x = player.x - 30;
+  if (keyDown("LEFT_ARROW") && player.x > width / 5 && state === "PLAY") {
+    player.x = player.x - 20;
     player.addImage(shooterImg2)
     position = "Left"
   }
 
-  if (keyDown("RIGHT_ARROW")) {
-    player.x = player.x + 30;
+  if (keyDown("RIGHT_ARROW") && player.x < 8/10 * width && state === "PLAY") {
+    player.x = player.x + 20;
     player.addImage(shooterImg)
     position = "Right"
   }
 
-  if (frameCount % 200 === 0) {
+  if(player.collide(zombieGroup)){
+    health -= 1;
+    zombieGroup.destroyEach()
+    player.velocityX = 0;
+  }
+
+  if(player.collide(coin)){
+    if(coinsCollected < 2){
+      moveCoin();
+    }else{
+      coin.destroy()
+    }
+    coinsCollected += 1;
+    newPlayer.updateCoins(coinsCollected)
+  }
+
+  if(coinsCollected === 3 && percent > 0.70){
+    treasure.visible = true;
+  }else{
+    treasure.visible = false;
+  }
+
+  if(player.collide(treasure) && treasure.visible === true){
+    state = "WON"
+    newPlayer.updateTreasure()
+    treasure.addImage(treasureOpenImg)
+    treasure.scale = 0.5
+    treasure1.addImage(treasureImg)
+    setTimeout(gameWon,3000)
+
+    zombieGroup.destroyEach()
+  }
+
+  switch(health){
+    case 2 : healthSprite.addImage(healthImg2)
+    break;
+    case 1 : healthSprite.addImage(healthImg1)
+    break;
+    case 0 : 
+    healthSprite.visible = false;
+    gameOver('heart')
+    break;
+  }
+
+  if (frameCount % 200 === 0 && state === "PLAY") {
     var randomNumber = Math.round(random(1, 2))
     if (randomNumber === 1) {
       zombie = createSprite(0, player.y, 50, 50);
@@ -96,12 +267,16 @@ function draw() {
   if (keyWentDown("space")) {
     if (position === "Left") {
       player.addImage(shooter_shooting2)
-      bullet = createSprite(player.x - 30, player.y, 20, 20)
+      bullet = createSprite(player.x - 30, player.y - 20, 20, 20)
+      bullet.addImage(bulletLeftImg)
+      bullet.scale = 0.1
       bullet.velocityX = -6;
       bulletGroup.add(bullet);
     } else if (position === "Right") {
       player.addImage(shooter_shooting)
-      bullet = createSprite(player.x + 30, player.y, 20, 20)
+      bullet = createSprite(player.x + 30, player.y - 20, 20, 20)
+      bullet.addImage(bulletRightImg)
+      bullet.scale = 0.1
       bullet.velocityX = 6;
       bulletGroup.add(bullet);
     }
@@ -114,6 +289,25 @@ function draw() {
     } else if (position === "Left") {
       player.addImage(shooterImg2)
     }
+  }
+
+  //Set coin tracker on top
+  switch(coinsCollected){
+    case 1 : 
+    coin1.addImage(coinFullImg)
+    coin2.addImage(coinClearImg)
+    coin3.addImage(coinClearImg)
+    break;
+    case 2 : 
+    coin1.addImage(coinFullImg)
+    coin2.addImage(coinFullImg)
+    coin3.addImage(coinClearImg)
+    break;
+    case 3 : 
+    coin1.addImage(coinFullImg)
+    coin2.addImage(coinFullImg)
+    coin3.addImage(coinFullImg)
+    break;
   }
 
   if (bulletGroup.collide(zombieGroup)) {
@@ -135,7 +329,7 @@ function draw() {
   drawSprites();
 
   //Progress Bar
-  var percent = (boxPositions.length) / 45
+  percent = boxPositions.length/45;
   noFill()
   strokeWeight(5)
   rect(width / 2, height / 7, width / 2 + width / 350, height / 30 + width / 350)
@@ -145,21 +339,111 @@ function draw() {
 
   textSize(20)
   fill(255, 255, 255)
-  text("Collected: " + boxPositions.length + "/45", width / 5, 100)
   text(Math.round(percent * 100) + "%", width / 4 + (percent * width / 2) - width / 44, height / 7 + height / 180)
 }
+
+function startGame(){
+  waitText.hide()
+  loading.hide()
+  newPlayer.updateOpponent()
+  otherCoin1.visible = true;
+  otherCoin2.visible = true;
+  otherCoin3.visible = true;
+  otherTreasure.visible = true;
+  opponentNameTxt.show()
+}
+
+function gameWon(){
+  swal(
+    {
+      title : "You Won!",
+      imageUrl : "/assets/treasure.png",
+      imageSize : "150x150",
+      text : "Congrats! You collected the treasure!",
+      confirmButtonText: "Play Again!"
+    },
+    function(isConfirm){
+      if(isConfirm === true){
+        location.reload()
+      }
+    }
+  )
+}
+
+function playSound(){
+  playerCount += 1;
+  newPlayer.addPlayers(input.value(),playerCount)
+  sound.play()
+  input.hide()
+  playButton.hide()
+  waitText.html(`<center>Welcome ${input.value()}! <br><br> Waiting for another <br>player to join.<center>`)
+  waitText.show()
+  loading.show()
+}
+
+function soundChange(){
+console.log(sound.isPlaying())
+if(sound.isPlaying() === true){
+  sound.pause()
+}else{
+  sound.play()
+}
+}
+
+function extraPlayer(){
+  swal(
+    {
+      title : "Uh-Oh!",
+      imageUrl : "/assets/gameOverHeart.png",
+      imageSize : "150x150",
+      text : "There are already 2 players playing. Try another time.",
+      confirmButtonText: "Check Again!"
+    },
+    function(isConfirm){
+      if(isConfirm === true){
+        location.reload()
+      }
+    }
+  )
+}
+
+function gameOver(how){
+  var txt;
+  switch(how){
+    case 'heart': txt = "You ran out of hearts. Better luck next time!"
+      break;
+    case 'treasure': txt = "The other player won first. Better luck next time!"
+      break;
+  }
+  swal(
+    {
+      title : "Game Over!",
+      imageUrl : "/assets/gameOverHeart.png",
+      imageSize : "150x150",
+      text : txt,
+      confirmButtonText: "Play Again!"
+    },
+    function(isConfirm){
+      if(isConfirm === true){
+        location.reload()
+      }
+    }
+  )
+}
+
+
 function moveCoin() {
-  var x = 1
-  var y = 4
-  coinImg.position((x * (height/6)), (y * (height/6)))
-  console.log((x * (height/6)))
+  var x = Math.round(random(0,8)) 
+  var y = Math.round(random(0,4))
+  coin.x = x * height/7 + width/5
+  coin.y = y * height/7 + height/4
 }
 
 function zombieBoxes(value) {
   var positions = []
   var count = 0;
 
-  boxX = (Math.round((zombieGroup.get(value).x) / (height / 6))) - 3
+  boxX = (Math.round((zombieGroup.get(value).x + 24) / (height / 6))) - 3
   boxY = Math.round((zombieGroup.get(value).y) / (height / 6)) - 1
 
   positions.push(boxX)
@@ -187,12 +471,11 @@ function zombieBoxes(value) {
 function colorBoxes() {
   var positions = [];
   var count = 0;
-  var boxX = (Math.round((player.x) / (height / 6))) - 2
+  var boxX = (Math.round((player.x + 24) / (height / 6))) - 2
   var boxY = Math.round(player.y / (height / 6)) - 1
 
   positions.push(boxX)
   positions.push(boxY)
-  console.log(positions[0] + "," + positions[1])
 
   if (positions[0] > -1 && positions[1] > -1 && positions[0] < 9 && positions[1] < 5) {
     for (var i = 0; i < boxPositions.length; i++) {
@@ -203,7 +486,6 @@ function colorBoxes() {
 
     if (count === 0) {
       boxPositions.push(positions)
-      //console.log(boxPositions)
     }
   }
 
